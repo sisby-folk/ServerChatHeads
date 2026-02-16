c8ed33456268c09cb6800cbc6c7cf28ebf9d2216
sisby-folk <sleepingdragoninn@gmail.com>
2026-02-16 17:06:38 +1100

    use skin IDs, cache on message, transparent support, fix singleplayer

--- src/main/java/com/campersamu/chatheads/ChatHeads.java
+++ src/main/java/com/campersamu/chatheads/ChatHeads.java
@@ -4,30 +4,38 @@ import eu.pb4.placeholders.api.PlaceholderResult;
 import eu.pb4.placeholders.api.Placeholders;
 import eu.pb4.polymer.autohost.impl.AutoHost;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.UUID;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URI;
+import java.net.URLConnection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static net.minecraft.text.Text.literal;
 import static net.minecraft.text.TextColor.fromRgb;
 
 public class ChatHeads implements ModInitializer {
-    //region Constants
-    public static final String MODID = "chatheads", PLAYER = "player";
+    public static final String MODID = "chatheads";
+    public static final String PLAYER = "player";
+    private static final Identifier pixel = Identifier.of(MODID, "pixel");
+    private static final Identifier noxel = Identifier.of(MODID, "noxel");
+
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
-    public static final ChatHeadsConfig CONFIG = ChatHeadsConfig.createToml(FabricLoader.getInstance().getConfigDir(), ChatHeads.MODID, "config", ChatHeadsConfig.class);
+    public static final ChatHeadsConfig CONFIG = ChatHeadsConfig.createToml(FabricLoader.getInstance().getConfigDir(), "", ChatHeads.MODID, ChatHeadsConfig.class);
     public static final TextColor[][] DEFAULT_HEAD_TEXTURE = new TextColor[][]{   //hex 0xC01044 -> TextColor.fromRgb(0xC01044)
             {fromRgb(0x191919), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x191919)},
             {fromRgb(0x191919), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x0c0c0c), fromRgb(0x191919)},
@@ -41,11 +49,9 @@ public class ChatHeads implements ModInitializer {
             {fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0), fromRgb(0xffd7b0)},
     };
     public static final Text DEFAULT_HEAD = paintHead(DEFAULT_HEAD_TEXTURE);
-    public static final HashMap<UUID, TextColor[][]> HEAD_CACHE = new HashMap<>();
-    //endregion
+    public static final Map<String, TextColor[][]> HEAD_CACHE = new ConcurrentHashMap<>();
 
     @Override
-    @SuppressWarnings("UnstableApiUsage")
     public void onInitialize() {
         //Add Mod Resources to Polymer Resource Pack
         PolymerResourcePackUtils.addModAssets(MODID);
@@ -54,13 +60,13 @@ public class ChatHeads implements ModInitializer {
         Placeholders.register(Identifier.of(MODID, PLAYER), (ctx, arg) -> {
             if (ctx.gameProfile() == null || ctx.server().getUserCache() == null) return PlaceholderResult.value(DEFAULT_HEAD);
             if (arg == null || arg.isEmpty())
-                return PlaceholderResult.value(paintHead(HEAD_CACHE.getOrDefault(ctx.gameProfile().getId(), DEFAULT_HEAD_TEXTURE)));
+                return PlaceholderResult.value(paintHead(getPlayerHead(ctx.player())));
             final var playerProfile = ctx.server().getUserCache().findByName(arg);
-            return playerProfile.map(gameProfile -> PlaceholderResult.value(paintHead(HEAD_CACHE.getOrDefault(gameProfile.getId(), DEFAULT_HEAD_TEXTURE))))
+            return playerProfile.map(gameProfile -> PlaceholderResult.value(paintHead(getPlayerHead(ctx.player()))))
                     .orElseGet(() -> PlaceholderResult.value(DEFAULT_HEAD));
         });
 
-        if (!AutoHost.config.enabled && !FabricLoader.getInstance().isModLoaded("arte")) {
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER && !AutoHost.config.enabled && !FabricLoader.getInstance().isModLoaded("arte")) {
             LOGGER.warn("""
               #####################################
                 Polymer AutoHost is not enabled!
@@ -72,21 +78,100 @@ public class ChatHeads implements ModInitializer {
         }
     }
 
-    //region Util
+    private TextColor[][] getPlayerHead(ServerPlayerEntity player) {
+        String skinId;
+        try {
+            skinId = player.getServer().getSessionService().getTextures(player.getGameProfile()).skin().getHash();
+        } catch (Exception e) {
+            return DEFAULT_HEAD_TEXTURE;
+        }
+        return HEAD_CACHE.computeIfAbsent(skinId, this::getPlayerHeadImmediate);
+    }
+
+    private TextColor[][] getPlayerHeadImmediate(String hash) {
+        //get skin url
+        final String playerSkinUrl = CONFIG.url.value().replace("<id>", hash);
+
+        //pull the picture
+        final BufferedImage image;
+        try {
+            URLConnection conn = URI.create(playerSkinUrl).toURL().openConnection();
+            conn.setRequestProperty("User-Agent", "ServerChatHeads/1.0 (+https://github.com/sisby-folk/ServerChatHeads; <sleepingdragoninn@gmail.com>)");
+            image = ImageIO.read(conn.getInputStream());
+        } catch (Exception e) {
+            LOGGER.warn("Failed to get image for {}", hash);
+            LOGGER.warn(e.toString());
+            return DEFAULT_HEAD_TEXTURE;
+        }
+
+        //generate the head
+        final TextColor[][] playerHead = new TextColor[8][8];
+
+        boolean fullSkin = image.getWidth() == 64;
+
+        if (fullSkin) {
+            int faceX = 8;
+            int faceY = 8;
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    int rgb = image.getRGB(faceX + x, faceY + y);
+                    if (rgb != 0) playerHead[y][x] = fromRgb(rgb & 0xffffff);
+                }
+            }
+            int hatX = 40;
+            int hatY = 8;
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    int rgb = image.getRGB(hatX + x, hatY + y);
+                    if (rgb != 0) playerHead[y][x] = fromRgb(rgb & 0xffffff);
+                }
+            }
+        } else {
+            // look man we gotta do SOMETHING
+            // calculate the non-transparent square bounds
+            int startXY = Integer.MAX_VALUE;
+            int endXY = Integer.MIN_VALUE;
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    if (image.getRGB(x, y) != 0) {
+                        startXY = Math.min(startXY, Math.min(x, y));
+                        endXY = Math.max(endXY, Math.max(x, y));
+                    }
+                }
+            }
+            // grab roughly 8x8 pixels from it
+            if (startXY < endXY) {
+                float step = (endXY + 1 - startXY) / 8.0F;
+                for (int x = 0; x < 8; x++) {
+                    for (int y = 0; y < 8; y++) {
+                        int sourceX = MathHelper.clamp(startXY + Math.round((x + 0.49F) * step), startXY, endXY);
+                        int sourceY = MathHelper.clamp(startXY + Math.round((y + 0.49F) * step), startXY, endXY);
+                        int rgb = image.getRGB(sourceX, sourceY);
+                        if (rgb != 0) playerHead[y][x] = fromRgb(rgb & 0xffffff);
+                    }
+                }
+            }
+        }
+
+        return playerHead;
+    }
+
     public static @NotNull Text paintHead(TextColor[][] head) {
         MutableText text = Text.empty();
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
+                Identifier font = head[y][x] == null ? noxel : pixel;
+                TextColor color = head[y][x];
+
                 text = text
-                        .append(literal("" + (char) (((int) '\uF810') + y)).setStyle(Style.EMPTY.withColor(head[y][x]).withFont(Identifier.of(MODID, "pixel"))))
-                        .append(literal("\uE001").fillStyle(Style.EMPTY.withFont(Identifier.of(MODID, "pixel"))));
+                        .append(literal("" + (char) (((int) '\uF810') + y)).setStyle(Style.EMPTY.withColor(color).withFont(font)))
+                        .append(literal("\uE001").fillStyle(Style.EMPTY.withFont(font)));
             }
-            text = text.append(literal("\uE008").fillStyle(Style.EMPTY.withFont(Identifier.of(MODID, "pixel"))));
+            text = text.append(literal("\uE008").fillStyle(Style.EMPTY.withFont(pixel)));
         }
 
         text.append(literal("  "));
 
         return text;
     }
-    //endregion
 }
--- src/main/java/com/campersamu/chatheads/ChatHeadsConfig.java
+++ src/main/java/com/campersamu/chatheads/ChatHeadsConfig.java
@@ -7,10 +7,8 @@ import folk.sisby.kaleido.lib.quiltconfig.api.values.TrackedValue;
 public class ChatHeadsConfig extends ReflectiveConfig {
     @Comment({
             "The URL used for downloading player heads.",
-            "Please ensure the downloaded head is 8x8 pixels!",
-            "Set to \"mojang\" to use Mojang's Authlib to get the URL automatically.",
-            "\"<uuid>\" will be replaced with the player's UUID.",
-            "Example: https://crafatar.com/avatars/<uuid>?size=8&overlay"
+            "\"<id>\" will be replaced with the skin texture ID.",
+            "Example: https://crafatar.com/avatars/<id>?size=8&overlay"
     })
-    public final TrackedValue<String> url = value("mojang");
+    public final TrackedValue<String> url = value("http://textures.minecraft.net/texture/<id>");
 }
